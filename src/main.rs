5d7afe433733187565f9a51398dfee3af3b00459
Avramoniu Calin <clavramoniu@gmail.com>
2026-03-24 21:11:53 +0200

    test: Test ir sensor to check functionality

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 #![no_std]
 #![no_main]
 
-use defmt::info;
-use defmt_rtt as _;
+use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::gpio::{Input, Pull};
 use embassy_time::Timer;
-use panic_probe as _;
+use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    let mut led = Output::new(p.PA5, Level::Low, Speed::Low);
 
-    info!("proiect started");
+    // output senzor -> PA0
+    let senzor_ir = Input::new(p.PA0, Pull::None);
+
+    info!("Incepem citirea senzorului IR...");
 
     loop {
-        led.toggle();
-        info!("LED toggled");
-        Timer::after_millis(2000).await;
+        // Senzorul scoate LOW cand detecteaza un obstacol (lumina reflectata)
+        if senzor_ir.is_low() {
+            info!("OBSTACOL DETECTAT!");
+        } else {
+            // Senzorul scoate HIGH cand nu se reflecta lumina inapoi
+            info!("Drum liber...");
+        }
+
+        Timer::after_millis(100).await;
     }
 }
